c7a870e0c6ce830fe7210b8355b5a5788949c116
Marc-Alexandre Blanchard <marc.alexandre.blanchard@gmail.com>
2018-01-11 23:19:15 +0100

    Improve over data generation + data fill, readme and rework to come !

--- data/generate.py
+++ data/generate.py
@@ -1,43 +1,61 @@
 import json
 import csv
 import io
+import sys
+import os
 
-input = "dataset.csv"
-output = "data.json"
+input = ""
+if(len(sys.argv)>1):
+    if(os.path.isfile(sys.argv[1])):
+        input = sys.argv[1]
+        output = sys.argv[1].replace(".csv",".json")
+    else:
+        print(sys.argv[1]+" not found")
+        sys.exit()
+else:
+    print("You must provide a file as argument : 'python generate.py file.csv'")
+    sys.exit()
 
 from enum import Enum
 
 class FIELD_IDX(Enum):
     NAME=0
     TYPE=1
-    MONTH=2
-    URL=3
-    IMAGE=4
+    ORIGIN=2
+    MONTH=3
+    URL=4
+    IMAGE=5
 
 res = {}
 res["fruits"]=[]
 res["vegetables"]=[]
+res["mushrooms"]=[]
+res["cereals"]=[]
 
 with open(input, newline='', encoding='utf-8') as f:
-    reader = csv.reader(f,delimiter=';')
+    reader = csv.reader(f,delimiter=',')
     nb = 0
     for row in reader:
         if(nb!=0):
             item = {}
             item["name"]=row[FIELD_IDX.NAME.value]
             item["type"]=row[FIELD_IDX.TYPE.value]
+            item["origin"]=row[FIELD_IDX.ORIGIN.value]
             item["url"]=row[FIELD_IDX.URL.value]
-            item["image"]=row[FIELD_IDX.IMAGE.value]
-            item["month"]=[]
+            item["image_url"]=row[FIELD_IDX.IMAGE.value]
+            item["availability"]=[]
             months = str(row[FIELD_IDX.MONTH.value]).split(",")
             for month in months :
-                item["month"].append(int(month))
+                item["availability"].append(int(month))
 
             if(item["type"]=="fruit"):
                 res["fruits"].append(item)
-            else:
+            elif(item["type"]=="vegetable"):
                 res["vegetables"].append(item)
-
+            elif(item["type"]=="mushroom"):
+                res["mushrooms"].append(item)
+            elif(item["type"]=="cereal"):
+                res["cereals"].append(item)
         nb=nb+1
 
 with io.open(output, 'w', encoding='utf8') as f:
---
+++ src/assets/images/README.md
@@ -0,0 +1,7 @@
+Images has been found on : 
+
+1. https://www.pexels.com
+
+2. https://pixabay.com
+
+All images are choosen because of their licence [CC0](https://creativecommons.org/publicdomain/zero/1.0/deed.fr)
