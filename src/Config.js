963adc8453a99137422280f33537486e8d3003a9
Marc-Alexandre Blanchard <marc.alexandre.blanchard@gmail.com>
2018-01-21 16:50:44 +0100

    Start new way of thinking configuration, i18n, config and data are configurable without rebuilding the app.

--- src/Config.js
+++
@@ -1,5 +0,0 @@
-/**
- * convert an index to its month name
- */
-export const config = {
-};
---
+++ src/innerConfig.js
@@ -0,0 +1,5 @@
+/**
+ * inner config of the app melt with config of the app
+ */
+export const innerConfig = {
+};
